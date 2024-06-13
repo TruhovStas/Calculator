379cf2273b96a0214d542e5c6111a005e876bdaf
Роман Барковец <romanbarkovets@yandex.ru>
2024-06-13 15:09:19 +0300

    add funcs definitions

--- Calculator/Utilities/Calculator.cs
+++ Calculator/Utilities/Calculator.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Calculator.Utilities
 {
-    internal class Calculator
+    public static class Calculator
     {
+        /// <summary>
+        /// Method solve equations which consists of numbers and math operations
+        /// Based on Sort Station Alghorythm
+        /// </summary>
+        /// <param name="equation">Numbers and math operatons</param>
+        /// <returns>Result of math operations</returns>
+        public static double SolveEquation(string equation)
+        {
+            // convert to reverse polish notation
+            // calculate
+            throw new NotImplementedException();
+        }
+
+        private static string Convert2RPN(string infixNotationString)
+        {
+
+            throw new NotImplementedException();
+        }
+
     }
 }
