fcf222fb096009771fa5f4ce038ae5e5ef429adf
Роман Барковец <romanbarkovets@yandex.ru>
2024-06-13 21:48:17 +0300

    add equation class

---
+++ Calculator/Models/NumericEquation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+	public class NumericEquation
+	{
+		public string infixEquation { get; private set; }
+		public string postfixEquation { get; private set; }
+
+
+		public NumericEquation(string equation)
+		{
+			this.infixEquation = equation;
+			this.postfixEquation = Convert2Postfix();
+
+		}
+
+
+		private string Convert2Postfix()
+		{
+			List<string> tokenizedEquation = SeparateEquation();
+
+			List<string> outputString = new List<string>();
+			Stack<string> symbolBuffer = new Stack<string>(); // needs for temporary keeping symbols 
+
+			foreach (var token in infixNotationString)
+			{
+
+			}
+			throw new NotImplementedException();
+			//get number must be here
+			return outputString.ToString();
+		}
+
+
+		private static List<string> SeparateEquation()
+		{
+			//use this infix eq
+			//get number here
+			throw new NotImplementedException();
+		}
+	}
+
+}
--- Calculator/Utilities/Calculator.cs
+++ Calculator/Utilities/Calculator.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator.Utilities
 {
+    // should i not use token instead of some another definition???
     public static class Calculator
     {
         /// <summary>
@@ -14,18 +16,36 @@ namespace Calculator.Utilities
         /// </summary>
         /// <param name="equation">Numbers and math operatons</param>
         /// <returns>Result of math operations</returns>
-        public static double SolveEquation(string equation)
+        public static double SolveEquation(string equation) // completed
         {
             // convert to reverse polish notation
-            // calculate
+            List<string> separatedEquation = SeparateEquation(equation);
+			List<string> pnEquation = Calculator.Convert2PostfixNotation(separatedEquation);
+            double result = CalculatePostfixNotationEquation(pnEquation);
+            return result;
+        }
+
+
+      
+
+        private static double GetNumber() // used for getting fractional numbers
+        {
             throw new NotImplementedException();
         }
 
-        private static string Convert2RPN(string infixNotationString)
-        {
 
+        private static double CalculatePostfixNotationEquation(List<string> equationTokens) //this method should calculate such funcs like sin cos abs and take it from Math C#
+        {
             throw new NotImplementedException();
         }
 
     }
+
+    internal class Equation
+    {
+
+    }
+
+    // релазиовать методы
+    // реализовать метод польской нотации
 }
