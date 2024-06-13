c70c3b132a49d63bb81f67c9e66fe240b0ad3266
TruhovStas <truhovstas2003@gmail.com>
2024-06-13 14:16:09 +0300

    Create VariableDictionaryclass

---
+++ Calculator/Models/VariableDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    internal class VariableDictionary
+    {
+        private Dictionary<string, Variable> dict;
+        VariableDictionary()
+        {
+            dict = new Dictionary<string, Variable>();
+        }
+        public void Add(Variable func)
+        {
+            dict[func.Name] = func;
+        }
+        public Variable GetFunction(string name)
+        {
+            return dict[name];
+        }
+    }
+}
