68c32741a0998ca0411f827e90ff56b7591e665b
Роман Барковец <romanbarkovets@yandex.ru>
2024-06-14 16:58:01 +0300

    add function definitions

--- Calculator/Utilities/Validator.cs
+++ Calculator/Utilities/Validator.cs
@@ -8,5 +8,21 @@ namespace Calculator.Utilities
 {
     public static class Validator
     {
-    }
+        public static bool IsCorrectUserFunction()
+        {
+            throw new NotImplementedException();
+        }
+
+
+		public static bool IsCorrectUserVariable()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public static bool IsCorrectMathExpression()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
